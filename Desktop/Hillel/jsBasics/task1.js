73e718629386ac95e7a089c5f5779fd3af5bbcfd
Daria Tkachuk <dariaaa.tkachuk@gmail.com>
2024-11-04 23:52:21 +0100

    Hometask 4.1

---
+++ Desktop/Hillel/jsBasics/task1.js
@@ -0,0 +1,15 @@
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+// Оголошення змінних
+let myString = "Hello, AQA!"; // рядок
+let myNumber = 42; // число
+let myBoolean = true; // булевий тип
+let myNull = null; // null
+let myUndefined; // undefined (не ініціалізована змінна)
+
+// Виведення значень на консоль
+console.log("Рядок:", myString);
+console.log("Число:", myNumber);
+console.log("Булевий тип:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
