114af24c5523c45acea561c030d31db6b3513dd4
Daria Tkachuk <dariaaa.tkachuk@gmail.com>
2024-11-04 21:35:56 +0100

    Добавила aqa в основной репозиторий

---
+++ Desktop/Hillel/JS-practice/practice2.js
@@ -0,0 +1,65 @@
+/** 
+const UserName = 'John';
+let age = 15;
+let iserLastName;
+console.log(namee);
+console.log(age);
+
+age = 20;
+
+console.log(UserName);
+console.log(age);
+
+age = 'Twemty';
+console.log(age);
+**/
+//number
+let age = 25;
+console.log(typeof age);
+
+//string
+
+let UserName = 'Ivan';
+console.log(typeof UserName);
+
+//Boolean - logical -- true,false
+
+let car = true;
+let isUderLoggedIn = true;
+let isUserPostedPhoto = false;
+
+console.log(typeof isUderLoggedIn);
+
+//undefined
+
+let undefinedvariable;
+
+console.log(undefinedvariable);
+
+//NaN - not a number
+let result = 'text'/2;
+console.log(result);
+
+//infinity 
+console.log(10 / 0);
+
+//BigInt
+const bigValue = 1234567890987654323456789876543456789;
+
+console.log(bigValue);
+
+const bigValueOptimized = 1232345678909876543212345678n;
+console.log(bigValueOptimized +10n);
+
+//Symbol
+
+//const uniqValue = Symbol('Value');
+//const uniqValue2 = Symbol ('Value');
+
+//console.log (uniqValue === uniqValue2);
+
+//const person = [
+    age: 15,
+//    name: 'Patrick'
+
+let 
