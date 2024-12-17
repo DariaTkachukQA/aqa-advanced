a30ec350365ea64a00c0b2299853c60019a067d2
Daria Tkachuk <dariaaa.tkachuk@gmail.com>
2024-12-17 20:54:58 +0100

    commit with husky

---
+++ test1.js
@@ -0,0 +1,8 @@
+const longString =
+	'qwertzuiopüpwoeirutzghvfjdklsöaäsödlfkjghbvncm,x.y-x.c,mvnherfhefhdfdfjvjkdfvnkjdfnvkjdfnvkjdnvjkdnvkdnvknvdjvndkjvnkdjvnkdvnkdvndvndvkdfnvksdfncvkdfnvkdf'
+let userName = 'Test'
+const obj = {
+	username: 'Testproperty',
+}
+
+let userlastname
